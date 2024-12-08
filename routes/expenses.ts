e82742a34a2b133e7c6166862a448d3ce41e44e8
SomaTakata <somatakata@gmail.com>
2024-12-08 17:28:39 +0900

    経費のダミーデータを追加し、GETリクエストで返却するように修正

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,8 +1,20 @@
 import { Hono } from "hono";
 
+type Expenses = {
+  id: string;
+  title: string;
+  amount: number;
+};
+
+const fakeExpenses: Expenses[] = [
+  { id: "1", title: "Car Insurance", amount: 294.67 },
+  { id: "2", title: "Rent", amount: 1000 },
+  { id: "3", title: "Groceries", amount: 200 },
+];
+
 export const expensesRoute = new Hono()
   .get("/", (c) => {
-    return c.json({ expenses: [] });
+    return c.json({ expenses: fakeExpenses });
   })
   .post("/", (c) => {
     return c.json({});
