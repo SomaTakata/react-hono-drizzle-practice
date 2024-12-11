61e350a32277d6152adb69b11421276ac7bb741a
SomaTakata <somatakata@gmail.com>
2024-12-11 11:19:49 +0900

    React Queryを使用して支出合計の取得を最適化し、useEffectを削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import {
   Card,
@@ -8,25 +7,34 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+async function getTotalSpent() {
+  const res = await api.expenses["total-spent"].$get();
+  if (!res.ok) {
+    throw new Error("Failed to fetch total spent");
+  }
+  const data = await res.json();
+  return data;
+}
 
 function App() {
-  const [totalSpent, setTotalSpent] = useState(0);
+  const { isPending, error, data } = useQuery({
+    queryKey: ["get-query-spent"],
+    queryFn: getTotalSpent,
+  });
 
-  useEffect(() => {
-    async function fetchTotalSpent() {
-      const res = await api.expenses["total-spent"].$get();
-      const data = await res.json();
-      setTotalSpent(data.totalSpent);
-    }
-    fetchTotalSpent();
-  }, []);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <Card className="w-[350px] m-auto">
       <CardHeader>
         <CardTitle>Total Spent</CardTitle>
         <CardDescription>The total amount you've spent</CardDescription>
       </CardHeader>
-      <CardContent>{totalSpent}</CardContent>
+      <CardContent>{isPending ? "..." : data.totalSpent}</CardContent>
     </Card>
   );
 }
