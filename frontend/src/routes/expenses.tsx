76916cb1b20bd47540f0598fea4e845e2d7604a2
SomaTakata <somatakata@gmail.com>
2024-12-14 09:50:40 +0900

    ExpensesコンポーネントにAPIからのデータ取得機能を追加し、エラーハンドリングを実装

--- frontend/src/routes/expenses.tsx
+++ frontend/src/routes/expenses.tsx
@@ -1,9 +1,32 @@
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/expenses")({
   component: Expenses,
 });
+async function getAllExpenses() {
+  const res = await api.expenses.$get();
+  if (!res.ok) {
+    throw new Error("Failed to fetch total spent");
+  }
+  const data = await res.json();
+  return data;
+}
 
 function Expenses() {
-  return <div className="p-2">Hello from Expenses!</div>;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["get-all-expenses"],
+    queryFn: getAllExpenses,
+  });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="p-2">
+      <pre>{isPending ? "..." : JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
