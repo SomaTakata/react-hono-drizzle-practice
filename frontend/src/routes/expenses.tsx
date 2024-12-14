c406d14aa3ae160433d98e781e9252a12feea25a
SomaTakata <somatakata@gmail.com>
2024-12-14 09:58:31 +0900

    Expensesコンポーネントにテーブルを追加し、APIから取得したデータを表示する機能を実装

--- frontend/src/routes/expenses.tsx
+++ frontend/src/routes/expenses.tsx
@@ -1,11 +1,21 @@
 import { api } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export const Route = createFileRoute("/expenses")({
   component: Expenses,
 });
 async function getAllExpenses() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const res = await api.expenses.$get();
   if (!res.ok) {
     throw new Error("Failed to fetch total spent");
@@ -25,8 +35,28 @@ function Expenses() {
   }
 
   return (
-    <div className="p-2">
-      <pre>{isPending ? "..." : JSON.stringify(data, null, 2)}</pre>
+    <div className="p-2 max-w-3xl m-auto">
+      <Table>
+        <TableCaption>A list of all expenses.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Id</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isPending
+            ? "..."
+            : data?.expenses.map((expense) => (
+                <TableRow key={expense.id}>
+                  <TableCell className="font-medium">{expense.id}</TableCell>
+                  <TableCell>{expense.title}</TableCell>
+                  <TableCell>{expense.amount}</TableCell>
+                </TableRow>
+              ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
