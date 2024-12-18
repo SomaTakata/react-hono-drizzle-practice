642a36c312aa8ff0ef6b4a53a256cd62433121eb
SomaTakata <somatakata@gmail.com>
2024-12-19 08:27:02 +0900

    プロフィールコンポーネントにユーザー情報の取得機能を追加し、ローディングおよびエラーハンドリングを実装

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,9 +1,33 @@
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/profile")({
   component: Profile,
 });
 
+async function getCurrentUser() {
+  const res = await api.me.$get();
+  if (!res.ok) {
+    throw new Error("Failed to fetch total spent");
+  }
+  const data = await res.json();
+  return data;
+}
+
 function Profile() {
-  return <div className="p-2">Hello from Profile!</div>;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["get-query-spent"],
+    queryFn: getCurrentUser,
+  });
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>not logged in</div>;
+  }
+
+  return <div className="p-2">Hello {data.user.family_name}</div>;
 }
