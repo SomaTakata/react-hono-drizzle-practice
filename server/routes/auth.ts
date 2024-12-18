2db1f5d1737874277983656c6810ff5beb2ff880
SomaTakata <somatakata@gmail.com>
2024-12-19 07:34:41 +0900

    認証ルートにコールバック処理を追加し、コードを簡素化

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,14 +1,17 @@
 import { Hono } from "hono";
 import { kindeClient, sessionManager } from "../kinde";
 
-export const authRoute = new Hono();
-
-authRoute.get("/login", async (c) => {
-  const loginUrl = await kindeClient.login(sessionManager(c));
-  return c.redirect(loginUrl.toString());
-});
-
-authRoute.get("/register", async (c) => {
-  const registerUrl = await kindeClient.register(sessionManager(c));
-  return c.redirect(registerUrl.toString());
-});
+export const authRoute = new Hono()
+  .get("/login", async (c) => {
+    const loginUrl = await kindeClient.login(sessionManager(c));
+    return c.redirect(loginUrl.toString());
+  })
+  .get("/register", async (c) => {
+    const registerUrl = await kindeClient.register(sessionManager(c));
+    return c.redirect(registerUrl.toString());
+  })
+  .get("/callback", async (c) => {
+    const url = new URL(c.req.url);
+    await kindeClient.handleRedirectToApp(sessionManager(c), url);
+    return c.redirect("/");
+  });
