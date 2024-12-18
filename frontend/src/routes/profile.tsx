3d8d785f1609f891bcb554a3c3e82ccefebc05c9
SomaTakata <somatakata@gmail.com>
2024-12-19 08:34:53 +0900

    ユーザー情報の取得方法を更新し、クエリオプションを使用するように変更

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,4 +1,4 @@
-import { api } from "@/lib/api";
+import { userQueryOptions } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -6,20 +6,8 @@ export const Route = createFileRoute("/profile")({
   component: Profile,
 });
 
-async function getCurrentUser() {
-  const res = await api.me.$get();
-  if (!res.ok) {
-    throw new Error("Failed to fetch total spent");
-  }
-  const data = await res.json();
-  return data;
-}
-
 function Profile() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ["get-query-spent"],
-    queryFn: getCurrentUser,
-  });
+  const { isPending, error, data } = useQuery(userQueryOptions);
 
   if (isPending) {
     return <div>Loading...</div>;
