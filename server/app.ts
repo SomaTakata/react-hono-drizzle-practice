d7740bd8cac91c9a5256c566ae8e24b486e94bd5
SomaTakata <somatakata@gmail.com>
2024-12-19 07:58:48 +0900

    認証ルートのパスを修正し、ルートをルートディレクトリに変更

--- server/app.ts
+++ server/app.ts
@@ -13,7 +13,7 @@ app.get("/test", (c) => c.json({ message: "test" }));
 const apiRoutes = app
   .basePath("/api")
   .route("/expenses", expensesRoute)
-  .route("/authRoute", authRoute);
+  .route("/", authRoute);
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
