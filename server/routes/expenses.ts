602d2e31d71f3c8a747b4e9daffeeabb4a66617e
SomaTakata <somatakata@gmail.com>
2024-12-24 21:27:59 +0900

    expensesルートにおいて、expensesTableをexpenseTableにリファクタリングし、インポート文の順序を整理

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -1,12 +1,12 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
-import { z } from "zod";
+
 import { getUser } from "../kinde";
 
 import { db } from "../db";
-import { expenses as expensesTable } from "../db/schema/expenses";
-import { and, desc, eq, sum } from "drizzle-orm";
-import { pgTable } from "drizzle-orm/pg-core";
+import { expenses as expenseTable } from "../db/schema/expenses";
+import { eq, desc, sum, and } from "drizzle-orm";
+import { z } from "zod";
 
 const expenseSchema = z.object({
   id: z.number().int().positive().min(1),
@@ -29,9 +29,9 @@ export const expensesRoute = new Hono()
     const user = c.var.user;
     const expenses = await db
       .select()
-      .from(expensesTable)
-      .where(eq(expensesTable.userId, user.id))
-      .orderBy(desc(expensesTable.createdAt))
+      .from(expenseTable)
+      .where(eq(expenseTable.userId, user.id))
+      .orderBy(desc(expenseTable.createdAt))
       .limit(100);
     return c.json({ expenses: expenses });
   })
@@ -39,7 +39,7 @@ export const expensesRoute = new Hono()
     const expense = await c.req.valid("json");
     const user = c.var.user;
     const result = await db
-      .insert(expensesTable)
+      .insert(expenseTable)
       .values({
         ...expense,
         userId: user.id,
@@ -47,44 +47,47 @@ export const expensesRoute = new Hono()
       .returning();
 
     c.status(201);
-    return c.json(expense);
+    return c.json(result);
   })
   .get("/total-spent", getUser, async (c) => {
     const user = c.var.user;
     const result = await db
-      .select({ total: sum(expensesTable.amount) })
-      .from(expensesTable)
-      .where(eq(expensesTable.userId, user.id))
+      .select({ total: sum(expenseTable.amount) })
+      .from(expenseTable)
+      .where(eq(expenseTable.userId, user.id))
       .limit(1)
       .then((res) => res[0]);
-    return c.json({ result });
+    return c.json(result);
   })
   .get("/:id{[0-9]+}", getUser, async (c) => {
     const id = Number.parseInt(c.req.param("id"));
     const user = c.var.user;
-    const result = await db
+
+    const expense = await db
       .select()
-      .from(expensesTable)
-      .where(and(eq(expensesTable.userId, user.id), eq(expensesTable.id, id)))
-      .orderBy(desc(expensesTable.createdAt))
+      .from(expenseTable)
+      .where(and(eq(expenseTable.userId, user.id), eq(expenseTable.id, id)))
       .then((res) => res[0]);
 
-    if (!result) {
+    if (!expense) {
       return c.notFound();
     }
-    return c.json({ result });
+
+    return c.json({ expense });
   })
   .delete("/:id{[0-9]+}", getUser, async (c) => {
     const id = Number.parseInt(c.req.param("id"));
     const user = c.var.user;
-    const result = await db
-      .delete(expensesTable)
-      .where(and(eq(expensesTable.userId, user.id), eq(expensesTable.id, id)))
+
+    const expense = await db
+      .delete(expenseTable)
+      .where(and(eq(expenseTable.userId, user.id), eq(expenseTable.id, id)))
       .returning()
       .then((res) => res[0]);
 
-    if (!result) {
+    if (!expense) {
       return c.notFound();
     }
-    return c.json({ result });
+
+    return c.json({ expense: expense });
   });
